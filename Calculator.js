8567a53237e41b9b5d2f38cb7f5dab6d611bdd45
Bhushangv <110773878+Bhushangv@users.noreply.github.com>
2022-08-07 21:16:59 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,24 @@
+// Method 1
+let a = 10
+let b = 5
+Sum = a+b
+Sub = a-b
+Mul = a*b
+Div = a/b
+console.log("Method 1:")
+console.log(`Sum of ${a} and ${b} is ${Sum}`)
+console.log(`Subtraction of ${a} and ${b} is ${Sub}`)
+console.log(`Multiplication of ${a} and ${b} is ${Mul}`)
+console.log(`Division of ${a} and ${b} is ${Div}`)
+// Method 2
+console.log("Method 2:")
+console.log(a+b,a-b,a*b,a/b)
+// Method 3
+console.log("Method 3:")
+console.log(Sum,Sub,Mul,Div)
+// Method 4
+console.log("Method 4:")
+console.log(Sum)
+console.log(Sub)
+console.log(Mul)
+console.log(Div)
